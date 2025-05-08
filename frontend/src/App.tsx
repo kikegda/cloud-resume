acc594ce7c77ccbc573dd82b6cdffd7fa6310bd6
kikegda <egarciadealfonso@gmail.com>
2025-05-08 18:52:13 +0200

    feat: add frontend setup with React, Vite, and Tailwind CSS; update cloudbuild.yaml for deployment

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-3xl font-bold underline">
+        hola esto es una prueba
+      </div>
+    </>
+  )
+}
+
+export default App
