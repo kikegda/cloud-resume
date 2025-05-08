c3114cb1550b089d08153cb41b25616d1fa4f125
kikegda <egarciadealfonso@gmail.com>
2025-05-08 18:16:08 +0200

    feat: implement frontend build and deployment process with Vite and React

---
+++ cloud-resume-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="text-center text-3xl font-bold underline">
+        hola esto es una prueba
+      </div>
+    </>
+  )
+}
+
+export default App
